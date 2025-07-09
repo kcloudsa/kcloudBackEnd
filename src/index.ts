b610a6162cdad2032a519d5af7c4bde5c1bf6430
kcloudsa <Kcloud.sa@gmail.com>
2025-07-09 23:58:47 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import app from './app';
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
-});
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    /* eslint-disable no-console */
+    console.log(`Listening: http://localhost:${port}`);
+    /* eslint-enable no-console */
+  });
+}
+
+// Export for Vercel
+export default app;
